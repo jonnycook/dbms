f0eaf33e4162c144d98d2d450fc9683b53a552ce
Jonny <qubsoft@gmail.com>
2015-07-06 05:17:41 -0500

    .

--- core/databaseEngines/AddressesDatabaseEngine.class.php
+++ core/databaseEngines/AddressesDatabaseEngine.class.php
@@ -14,49 +14,59 @@ class AddressesDatabaseStorageEngine extends DatabaseEngine {
 
 	public function relationship(array $schema, $model, $id, array $storageConfig, $relName, array $relSchema, &$value) {
 		// return false;
-		$response = json_decode(file_get_contents('http://' . QS1_SERVER . '/api/Patient/' . QS1_PHARMACY . '/Addresses?patientID=YOUNDAV'), true);
-		foreach ($response as $i => $obj) {
-			$addresses[] = array(
-				'id' => $id . '-' . $obj['AddressID'],
-				'street1' => $obj['Address'],
-				'street2' => $obj['Address2'],
-				'city' => $obj['City'],
-				'state' => $obj['State'],
-				'zipCode' => $obj['Zip'],
-				'name' => $obj['Name'],
-				'user' => $id,
-			);
+		$user = _mongoClient()->divvydose->User->findOne(array('_id' => new MongoId($id)));
+		if ($user['patientId']) {
+			$response = json_decode(file_get_contents('http://' . QS1_SERVER . '/api/Patient/' . QS1_PHARMACY . '/Addresses?patientID=' . $user['patientId']), true);
+			foreach ($response as $i => $obj) {
+				$addresses[] = array(
+					'id' => $id . '-' . $obj['AddressID'],
+					'street1' => $obj['Address'],
+					'street2' => $obj['Address2'],
+					'city' => $obj['City'],
+					'state' => $obj['State'],
+					'zip' => $obj['Zip'],
+					'name' => $obj['Name'],
+					'user' => $id,
+				);
+			}
+			$value = $addresses;
+			return true;
 		}
-		$value = $addresses;
-		return true;
+		else {
+			return false;
+		}
 	}
 
 	public function insert(array $schema, array $storageConfig, $model, $id, array $changes) {
-		$fields = array(
-			'Address' => def($changes['attributes']['street1'], 'Address'),
-			'Address2' => def($changes['attributes']['street2'], ''),
-			'City' => def($changes['attributes']['city'], 'City'),
-			'State' => def($changes['attributes']['state'], 'SS'),
-			'Zip' => def($changes['attributes']['zipCode'], '12345'),
-			'Name' => def($changes['attributes']['name'], 'Name'),
-			'PatientID' => 'YOUNDAV'
-		);
+		$user = _mongoClient()->divvydose->User->findOne(array('_id' => new MongoId($changes['relationships']['user'])));
 
-		$fieldsStr = array();
+		if ($user['patientId']) {
+			$fields = array(
+				'Address' => def($changes['attributes']['street1'], 'Address'),
+				'Address2' => def($changes['attributes']['street2'], ''),
+				'City' => def($changes['attributes']['city'], 'City'),
+				'State' => def($changes['attributes']['state'], 'SS'),
+				'Zip' => def($changes['attributes']['zip'], '12345'),
+				'Name' => def($changes['attributes']['name'], 'Name'),
+				'PatientID' => $user['patientId'],
+			);
 
-		foreach ($fields as $key => $value) {
-			$fieldsStr[] = "$key=$value";
+			$fieldsStr = array();
+
+			foreach ($fields as $key => $value) {
+				$fieldsStr[] = "$key=$value";
+			}
+			$fieldsStr = implode('&', $fieldsStr);
+
+			$ch = curl_init('http://' . QS1_SERVER . '/api/Patient/' . QS1_PHARMACY . '/Addresses');
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsStr);
+			$response = curl_exec($ch);
+			$response = json_decode($response, true);
+
+			return "{$changes['relationships']['user']}-$response[AddressID]";
 		}
-		$fieldsStr = implode('&', $fieldsStr);
-
-		$ch = curl_init('http://' . QS1_SERVER . '/api/Patient/' . QS1_PHARMACY . '/Addresses');
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsStr);
-		$response = curl_exec($ch);
-		$response = json_decode($response, true);
-
-		return "{$changes['relationships']['user']}-$response[AddressID]";
 	}
 
 	public function update(array $schema, array $storageConfig, $model, $id, array $changes) {
@@ -66,7 +76,7 @@ class AddressesDatabaseStorageEngine extends DatabaseEngine {
 				case 'street2': $fields['Address2'] = def($value, ''); break;
 				case 'city': $fields['City'] = def($value, 'SS'); break;
 				case 'state': $fields['State'] = def($value, 'City'); break;
-				case 'zipCode': $fields['Zip'] = def($value, '12345'); break;
+				case 'zip': $fields['Zip'] = def($value, '12345'); break;
 				case 'name': $fields['Name'] = def($value, 'Name'); break;
 			}
 		}
