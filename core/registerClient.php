f4c28cb2efbb3e2ad84f791a34b7b7fafefd393f
Jonny <qubsoft@gmail.com>
2015-07-16 20:55:42 -0500

    .

--- core/registerClient.php
+++ core/registerClient.php
@@ -14,7 +14,14 @@ if ($_GET['params']) {
 	$params = json_decode($_GET['params']);
 }
 
-$mongo->clients->insert(array('_id' => $clientId, 'token' => $_GET['token'], 'opts' => $opts, 'subscribedTo' => array(), 'params' => $params));
+$mongo->clients->insert(array(
+	'_id' => $clientId,
+	'token' => $_GET['token'],
+	'opts' => $opts,
+	'subscribedTo' => array(),
+	'params' => $params,
+	'registeredAt' => gmdate('Y-m-d H:i:s'),
+));
 
 echo json_encode(array(
 	'id' => $clientId
