86229f93a7e86a18a15e3747119404ae63b32b9e
Jonny <qubsoft@gmail.com>
2015-07-24 15:11:22 -0500

    .

--- core/clientConnected.php
+++ core/clientConnected.php
@@ -3,4 +3,4 @@
 require_once('includes/header.php');
 
 $mongo = mongoClient();
-$mongo->clients->update(array('_id' => $_GET['id']), array('$set' => array('connected' => true)));
+var_dump($mongo->clients->update(array('_id' => $_GET['id']), array('$set' => array('connected' => true))));
