b901f88bbef3b76cbbd8a84be6f60748793e1e53
Jonny <qubsoft@gmail.com>
2015-07-25 20:55:05 -0500

    .

--- core/databases/divvydose.php
+++ core/databases/divvydose.php
@@ -109,8 +109,7 @@ return array(
 					if ($user['patientId'] == 'DUMMY') {
 						// $date = ;
 						$timezone = date_default_timezone_get();
-						var_dump(timezone_name_from_abbr('', 300*3600));
-						date_default_timezone_set(timezone_name_from_abbr('', $user['timezone']*3600));
+						date_default_timezone_set(timezone_name_from_abbr('', -$user['timezone']*60, 0));
 						$user['divvyPacks'] = array(
 			        date('Y-m-d') => array(
 			            "prescriptions" => array(
