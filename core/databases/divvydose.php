1eb3523771eabb94ec4f57e3be0e867e390fea1c
Jonny <qubsoft@gmail.com>
2015-07-25 21:05:44 -0500

    .

--- core/databases/divvydose.php
+++ core/databases/divvydose.php
@@ -156,7 +156,7 @@ return array(
 			            ), 
 			            "packets" => array(
 			                array(
-			                    "time" => date('Y-m-d H:i:s', mktime(date('H') + 1, date('i'), date('s'), date('m'), date('d'), date('Y'))), 
+			                    "time" => date('Y-m-d H:i:s', mktime(date('H') + 1, 0, 0, date('m'), date('d'), date('Y'))), 
 			                    "doses" => array(
 			                        array(
 			                            "rxNumber" => "1", 
